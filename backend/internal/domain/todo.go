abe9f9ebc033c40ea41ecfcf1b6261172d1a615b
wiwatsapon <wiwatsapon.l@kanda.digital>
2025-03-17 15:56:48 +0700

    :hammer:(Domain):Add Priority and Difficulty

--- backend/internal/domain/todo.go
+++ backend/internal/domain/todo.go
@@ -6,6 +6,8 @@ type Todo struct {
 	ID          uint      `json:"id" gorm:"primaryKey"`
 	Title       string    `json:"title" gorm:"not null"`
 	Description string    `json:"description"`
+	Priority    uint      `json:"priority" gorm:"default:1;check:priority >= 1 AND priority <= 3"`       // 1:low, 2:medium, 3:high
+	Difficulty  uint      `json:"difficulty" gorm:"default:1;check:difficulty >= 1 AND difficulty <= 3"` // 1:easy, 2:medium, 3:hard
 	Completed   bool      `json:"completed" gorm:"default:false"`
 	CreatedAt   time.Time `json:"created_at"`
 	UpdatedAt   time.Time `json:"updated_at"`
